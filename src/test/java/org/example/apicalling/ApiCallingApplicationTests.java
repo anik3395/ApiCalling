dd1de8025401de48c9ca0d49ab780985af8367ed
anik <dascuet18.bd@gmail.com>
2026-04-23 17:18:28 +0600

    first commit

---
+++ src/test/java/org/example/apicalling/ApiCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apicalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCallingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
